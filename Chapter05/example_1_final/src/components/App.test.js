c55bfe0747fae82eb6e66a6d4343cebe7e6c29a6
GersonEC <gersonenriquez94@gmail.com>
2022-04-13 13:55:08 +0200

    AAA

--- Chapter05/example_1_final/src/components/App.test.js
+++ Chapter05/example_1_final/src/components/App.test.js
@@ -1,24 +1,24 @@
-import { render, screen, within } from '@testing-library/react';
-import user from '@testing-library/user-event';
-import App from './App';
+import { render, screen, within } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import App from "./App";
 
-describe('Integration: Budget App', () => {
+describe("Integration: Budget App", () => {
   function setOneDollarIncome() {
     user.click(screen.getByText(/set income/i));
-    user.type(screen.getByRole('spinbutton'), '1');
+    user.type(screen.getByRole("spinbutton"), "1");
     user.click(screen.getByText(/submit/i));
   }
 
-  function createCarBudget(amount = '5') {
+  function createCarBudget(amount = "5") {
     user.click(screen.getByText(/create new budget/i));
-    user.selectOptions(screen.getByRole('combobox', { name: /category/i }), [
-      screen.getByText('Auto'),
+    user.selectOptions(screen.getByRole("combobox", { name: /category/i }), [
+      screen.getByText("Auto"),
     ]);
-    user.type(screen.getByRole('spinbutton'), amount);
+    user.type(screen.getByRole("spinbutton"), amount);
     user.click(screen.getByText(/add budget/i));
   }
 
-  test('SetIncome, given income amount, sets income', () => {
+  test("SetIncome, given income amount, sets income", () => {
     render(<App />);
 
     setOneDollarIncome();
@@ -27,18 +27,18 @@ describe('Integration: Budget App', () => {
 
     expect(leftOverBudgetAmount).toBeInTheDocument();
     expect(
-      screen.getByRole('heading', { name: /income: \$1/i })
+      screen.getByRole("heading", { name: /income: \$1/i })
     ).toBeInTheDocument();
   });
 
-  describe('CreateNewBudget', () => {
+  describe("CreateNewBudget", () => {
     test.each`
       budgetAmount | spending           | leftOver
-      ${'4'}       | ${'Spending: $5'}  | ${'$-4'}
-      ${'5'}       | ${'Spending: $5'}  | ${'$-4'}
-      ${'6'}       | ${'Spending: $10'} | ${'$-9'}
+      ${"4"}       | ${"Spending: $5"}  | ${"$-4"}
+      ${"5"}       | ${"Spending: $5"}  | ${"$-4"}
+      ${"6"}       | ${"Spending: $10"} | ${"$-9"}
     `(
-      'given budget, updates budget summary',
+      "given budget, updates budget summary",
       ({ budgetAmount, spending, leftOver }) => {
         render(<App />);
         setOneDollarIncome();
@@ -49,64 +49,64 @@ describe('Integration: Budget App', () => {
 
         expect(leftOverBudgetAmount).toBeInTheDocument();
         expect(
-          screen.getByRole('heading', { name: spending })
+          screen.getByRole("heading", { name: spending })
         ).toBeInTheDocument();
       }
     );
-    test('given budget, displays budget chart', () => {
+    test("given budget, displays budget chart", () => {
       render(<App />);
       setOneDollarIncome();
       createCarBudget();
 
-      expect(screen.getByTestId('chart')).toBeInTheDocument();
+      expect(screen.getByTestId("chart")).toBeInTheDocument();
     });
   });
-  describe('Budget', () => {
-    test('given budget, displays details', () => {
+  describe("Budget", () => {
+    test("given budget, displays details", () => {
       render(<App />);
       setOneDollarIncome();
       createCarBudget();
 
-      const budgetList = screen.getByRole('listitem');
+      const budgetList = screen.getByRole("listitem");
 
       expect(within(budgetList).getByText(/auto/i)).toBeInTheDocument();
       expect(
-        screen.getByRole('heading', { name: /\$0 of \$5/i })
+        screen.getByRole("heading", { name: /\$0 of \$5/i })
       ).toBeInTheDocument();
     });
 
-    test('given budget expense, updates budget progress', () => {
+    test("given budget expense, updates budget progress", () => {
       render(<App />);
       setOneDollarIncome();
       createCarBudget();
 
-      user.click(screen.getByRole('button', { name: /arrowright/i }));
+      user.click(screen.getByRole("button", { name: /arrowright/i }));
 
       expect(
-        screen.getByRole('heading', { name: /\$5 of \$5/i })
+        screen.getByRole("heading", { name: /\$5 of \$5/i })
       ).toBeInTheDocument();
     });
   });
 
-  test('DeleteBudget, given deleted budget, budget removed from DOM', () => {
+  test("DeleteBudget, given deleted budget, budget removed from DOM", () => {
     render(<App />);
     setOneDollarIncome();
     createCarBudget();
 
     user.click(screen.getByLabelText(/trash can/i));
 
-    expect(screen.queryByRole('listitem')).not.toBeInTheDocument();
+    expect(screen.queryByRole("listitem")).not.toBeInTheDocument();
   });
 });
 
-it('SetIncome, given initial render, displays budget summary values', () => {
+it("SetIncome, given initial render, displays budget summary values", () => {
   render(<App />);
 
-  const income = screen.getByRole('heading', { name: /income: \$0/i });
-  const spending = screen.getByRole('heading', { name: /spending: \$0/i });
+  const income = screen.getByRole("heading", { name: /income: \$0/i });
+  const spending = screen.getByRole("heading", { name: /spending: \$0/i });
   const leftover = screen.getByText(/left over:/i);
 
-  expect(income).toHaveTextContent('Income: $0');
-  expect(spending).toHaveTextContent('Spending: $0');
-  expect(leftover).toHaveTextContent('$0');
+  expect(income).toHaveTextContent("Income: $0");
+  expect(spending).toHaveTextContent("Spending: $0");
+  expect(leftover).toHaveTextContent("$0");
 });
--- Chapter05/example_1_starter/src/components/App.test.js
+++ Chapter05/example_1_starter/src/components/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render, screen, within } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Test Legacy application", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  function setOneDollarIncome() {
+    user.click(screen.getByText(/set income/i));
+    user.type(screen.getByRole("spinbutton"), "1");
+    user.click(screen.getByText(/submit/i));
+  }
+
+  test("SetIncome, given income amount, sets income", () => {
+    render(<App />);
+    setOneDollarIncome();
+    const leftOverBudget = screen.getByText(/left over:/i);
+    const leftOverBudgetAmount = within(leftOverBudget).getByText(/\$1/i);
+    expect(leftOverBudgetAmount).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /income: \$1/i })
+    ).toBeInTheDocument();
+  });
 });
